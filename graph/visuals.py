3ca17edb21d05f0bdfd549566bdf055e558a2858
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-08-30 15:57:16 -0500

    Starting Fresh

--- graph/visuals.py
+++
@@ -1,9 +0,0 @@
-import colorsys
-
-def get_color(n: int, tot_colors: int) -> tuple[int, int, int]:
-    hue = n / tot_colors  # evenly spaced hue
-    # pastel settings: lower saturation, higher value
-    sat = 0.4
-    val = 0.9
-    r, g, b = colorsys.hsv_to_rgb(hue, sat, val)
-    return (int(r * 255), int(g * 255), int(b * 255))
--- helper/constants.py
+++
@@ -1,9 +0,0 @@
-# operations
-ROWS = 4
-PENALTY_MIN = 1e4
-PENALTY_MAX = 1e9
-
-
-# debugging
-DRAW_FORCE = True
-DEBUG_TIMING = True
